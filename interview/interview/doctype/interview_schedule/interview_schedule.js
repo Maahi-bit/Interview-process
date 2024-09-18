3011c117a870bcd092569d7a4888b3bfb4fef561
Maahi-git <shahimahishahi040@gmail.com>
2024-09-18 17:04:47 +0530

    first commit

---
+++ interview/interview/doctype/interview_schedule/interview_schedule.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2024, MS and contributors
+// For license information, please see license.txt
+
+// frappe.ui.form.on("Interview Schedule", {
+// 	refresh(frm) {
+
+// 	},
+// });
---
+++ interview/interview/doctype/interview_schedule/test_interview_schedule.py
@@ -0,0 +1,9 @@
+# Copyright (c) 2024, MS and Contributors
+# See license.txt
+
+# import frappe
+from frappe.tests.utils import FrappeTestCase
+
+
+class TestInterviewSchedule(FrappeTestCase):
+	pass
